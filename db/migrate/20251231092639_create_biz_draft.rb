3bca930f09ab593f1e5cccad22be6af4edfa1477
freeshenls <freeshenls@163.com>
2026-02-27 15:43:58 +0800

    修改draft长度

--- db/migrate/20251231092639_create_biz_draft.rb
+++ db/migrate/20251231092639_create_biz_draft.rb
@@ -1,8 +1,8 @@
 class CreateBizDraft < ActiveRecord::Migration[8.1]
   def change
     create_table :biz_draft do |t|
-      t.string :name, limit: 255
-      t.string :file_type, limit: 20
+      t.text :name
+      t.text :file_type
       t.text :file_path
       t.datetime :upload_time, default: -> { "now()" }
 
