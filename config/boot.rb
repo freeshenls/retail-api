f27a97fc4bc9a734b51107ea5ccb5a8825452b48
freeshenls <freeshenls@163.com>
2026-02-27 15:47:52 +0800

    修改boot

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,4 @@
-ENV["RAILS_MASTER_KEY"] = "a77c752e1b1ffd57e4e1baf4b110bbd1"
+ENV["RAILS_MASTER_KEY"] = "eb820d7c2694b34b428ffce968221522"
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
