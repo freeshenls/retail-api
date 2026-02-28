6cfabca96e3b41a3049482ad2aab0932adfce866
freeshenls <freeshenls@163.com>
2026-02-28 16:38:03 +0800

    修改bug

--- app/models/biz/customer.rb
+++ app/models/biz/customer.rb
@@ -1,5 +1,7 @@
 class Biz::Customer < ApplicationRecord
 
-	has_many :customer_users
+	has_many :customer_users, dependent: :destroy
 	has_many :users, through: :customer_users
+
+	has_many :orders, dependent: :destroy
 end
